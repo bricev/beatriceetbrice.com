bd435968dea80cde2b74555394360bf0c08b3a91
brice <brice@libcast.com>
2019-03-20 21:43:23 +0100

    move back & front sources into a same root dir

--- src/backend/Application/Http/Controller/StripeController.php
+++ src/backend/Application/Http/Controller/StripeController.php
@@ -27,16 +27,16 @@ final class StripeController
 
         /** @var Session $session */
         $session = Session::create([
-            'success_url'          => 'https://beatriceetbrice.com/#/stripe-success',
-            'cancel_url'           => 'https://beatriceetbrice.com/#/stripe-cancel',
+            'success_url'          => 'https://beatriceetbrice.com/honeymoon/success',
+            'cancel_url'           => 'https://beatriceetbrice.com/honeymoon/cancel',
             'payment_method_types' => ['card'],
             'line_items'           => [[
                 'amount'           => $amount,
                 'currency'         => 'eur',
                 'quantity'         => 1,
                 'name'             => 'Béatrice & Brice Honey Moon',
-                'description'      => 'Don offert dans le cadre du mariage de Béatrice Menu et Brice Vercoustre afin de participer au financement du voyage de noces.',
-                'images'           => ['https://beatriceetbrice.com/img/stripe_bb.jpg'],
+                'description'      => 'Participation au financement du voyage de noces de Béatrice Menu et Brice Vercoustre.',
+                'images'           => ['https://beatriceetbrice.com/assets/img/stripe_bb.jpg'],
             ]]
         ], [
             'stripe_version' => '2018-11-08; checkout_sessions_beta=v1'
